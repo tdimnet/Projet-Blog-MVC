609ea1336ecc9ffc906304908bd83eb5e449d064
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-05-25 09:39:14 +0200

    Add an if/else statement in order to connect and disconnect the user.

--- Vue/Templates/footer.php
+++ Vue/Templates/footer.php
@@ -2,7 +2,15 @@
 
 <footer>
   <h1>Here goes the footer</h1>
-  <a href="?Controller=Admin">Se connecter</a>
+
+  <?php
+  if ($path === 'Controller/Blog/index.php') {
+  ?>
+    <a href="?Controller=Admin">Se connecter</a>
+  <?php } else { ?>
+    <a href="?Controller=Blog">Se deconnecter</a>
+  <?php } ?>
+
 </footer>
 
 </body>
