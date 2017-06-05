6dbec7eb259650590f65ade9c318e895efb70061
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-06-05 23:28:50 +0200

    Style the leaving comment form

--- Vue/Blog/leaveComment.php
+++ Vue/Blog/leaveComment.php
@@ -1,12 +1,18 @@
 <div class="container">
   <h2>Leave a comment</h2>
   <form class="" method="post">
-    <input type="text" name="name" placeholder="Your name">
-    <br>
-    <input type="text" name="mail" placeholder="Your e-mail address">
-    <br>
-    <textarea name="comment" rows="8" cols="80" placeholder="Your comment"></textarea>
-    <br>
-    <button type="submit" name="button">Send your comment</button>
-  </form>  
+    <div class="form-group">
+      <label for="fullname">Your name :</label>
+      <input id="fullname" class="form-control" type="text" name="name" placeholder="Your name">
+    </div>
+    <div class="form-group">
+      <label for="email">Your e-mail address</label>
+      <input id="email" class="form-control" type="email" name="mail" placeholder="Your e-mail address">
+    </div>
+    <div class="form-group">
+      <label for="comment">Your comment</label>
+      <textarea id="comment" class="form-control" name="comment" placeholder="Your comment"></textarea>
+    </div>
+    <button class="btn btn-default" type="submit" name="button">Send your comment</button>
+  </form>
 </div>
