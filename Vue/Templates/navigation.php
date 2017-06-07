2d5d2642c82103fcd37dec1c03311868c4802db9
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-06-07 16:39:03 +0200

    Fix the header bug: when a user is on the admin panel, the article title is not show

--- Vue/Templates/navigation.php
+++ Vue/Templates/navigation.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <a class="navbar-brand" href="./">Billet simple pour l'Alaska</a>
+    <?php
+    if ($_GET['Controller'] === 'Blog') {
+    ?>
     <ul class="nav navbar-nav navbar-left">
       <li>
         <a href="?Controller=Blog&&Vue=article&&id=<?php echo $article['id']; ?>">
@@ -8,6 +11,9 @@
         </a>
     </li>
     </ul>
+    <?php
+    }
+    ?>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="./">Retourner à l'accueil</a></li>
     </ul>
