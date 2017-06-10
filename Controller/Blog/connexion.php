14a77f3fd0897f78c9f2e569c46d935334da24d3
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-06-10 23:30:02 +0200

    Correct bug with connexion page

--- Controller/Blog/connexion.php
+++ Controller/Blog/connexion.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'Vue/Blog/connexion.php';
 
-if (isset($_POST)) {
+if (isset($_POST) && !empty($_POST)) {
   $identifier = htmlspecialchars($_POST['identifier']);
   $password = htmlspecialchars($_POST['password']);
   if (isset($identifier) && isset($password)) {
-    header('Location: index.php?Controller=Admin');
+    var_dump($_POST);
   }
 }
