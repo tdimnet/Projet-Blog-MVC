c1c027e5a964700b2ba8ed06a4d3522bbc480c47
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-07-05 22:57:30 +0200

    Fix design

--- Vue/Admin/allComments.php
+++ Vue/Admin/allComments.php
@@ -14,32 +14,33 @@ include_once 'Vue/Templates/navigation.php';
 </div>
 
 
+<div class="container">
 <?php
 foreach ($allComments as $comment) { ?>
-  <div class="col-sm-6 col-md-4 col-lg-3">
-    <h4>
-      Comment author : <?php echo $comment->getFull_name() ?>
-      <br>
-    </h4>
-    <p>
-      Commnent :
-      <br>
-      <?php echo $comment->getComment() ?>
-    </p>
-    <p>
-      <a
-        class="btn btn-danger"
-        href="?Controller=Admin&&Action=moderateComment&&id=<?php echo $comment->getId(); ?>"
-      >
-        Moderate the article comment
-      </a>
-    </p>
-    <hr>
-  </div>
 
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <h4>
+        Comment author : <?php echo $comment->getFull_name() ?>
+        <br>
+      </h4>
+      <p>
+        Commnent :
+        <br>
+        <?php echo $comment->getComment() ?>
+      </p>
+      <p>
+        <a
+          class="btn btn-danger"
+          href="?Controller=Admin&&Action=moderateComment&&id=<?php echo $comment->getId(); ?>"
+        >
+          Moderate the article comment
+        </a>
+      </p>
+    </div>
 <?php
 }
 ?>
+</div>
 
 
 
