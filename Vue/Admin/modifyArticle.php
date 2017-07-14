a77c81cd7fb38c86fd0748c0b3aea03e8bb47dba
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-07-14 16:29:40 +0200

    Changed the wording for the new and modify article section

--- Vue/Admin/modifyArticle.php
+++ Vue/Admin/modifyArticle.php
@@ -4,27 +4,27 @@ include_once 'Vue\Templates\navigation.php';
 ?>
 
 <div class="container">
-  <h2>Modify an existing article</h2>
+  <h2>Modifier votre article existant</h2>
   <hr>
   <form class="" method="post">
     <div class="form-group">
       <label for="titre">Titre de l'episode</label>
-      <input id="titre" class="form-control" type="text" name="titre" placeholder="Episode title" value="<?php echo $article->getTitre(); ?>">
+      <input id="titre" class="form-control" type="text" name="titre" placeholder="Titre de l'episode" value="<?php echo $article->getTitre(); ?>">
     </div>
     <div class="form-group">
       <label for="episode">Contenu de l'episode</label>
-      <textarea id="episode" class="form-control" name="episode" placeholder="Episode text"><?php echo $article->getEpisode(); ?></textarea>
+      <textarea id="episode" class="form-control" name="episode"><?php echo $article->getEpisode(); ?></textarea>
     </div>
     <div class="form-group">
       <label for="status">Status de l'episode</label>
       <select id="status" class="form-control" name="status">
-        <option value="0">Not Publish</option>
-        <option value="1">Publish</option>
+        <option value="0">Mettre l'article en attente</option>
+        <option value="1">Publier l'article</option>
       </select>
     </div>
     <input type="hidden" name="token" id="token" value="<?php echo $token; ?>" />
     <button class="btn btn-default" type="submit" name="button">
-      Modify your episode
+      Modifier votre article
     </button>
   </form>
 </div>
--- Vue/Admin/newArticle.php
+++ Vue/Admin/newArticle.php
@@ -4,29 +4,30 @@ include_once 'Vue\Templates\navigation.php';
 ?>
 
 <div class="container">
-  <h2>Write a new article</h2>
+  <h2>
+    Ecrire un nouvel article
+  </h2>
   <hr>
 
   <form class="" method="post">
     <div class="form-group">
       <label for="titre">Titre de l'épisode</label>
-      <input id="titre" class="form-control" type="text" name="titre" placeholder="Episode title">
+      <input id="titre" class="form-control" type="text" name="titre" placeholder="Titre de l'épisode">
     </div>
     <div class="form-group">
-      <label for="episode">Votre episode</label>
-      <textarea id="episode" class="form-control" name="episode">
-      </textarea>
+      <label for="episode">Texte de l'épisode</label>
+      <textarea id="episode" class="form-control" name="episode"></textarea>
     </div>
     <div class="form-group">
       <label for="status">Souhaitez-vous publier l'article ?</label>
       <select id="status" class="form-control" name="status">
-        <option value="0">Not Publish</option>
-        <option value="1">Publish</option>
+        <option value="0">Mettre l'article en attente</option>
+        <option value="1">Publier l'article</option>
       </select>
     </div>
     <input type="hidden" name="token" id="token" value="<?php echo $token; ?>" />
     <button type="submit" class="btn btn-default" name="button">
-      Save/Send your episode
+      Sauvegarder/Publier votre épisoe
     </button>
   </form>
 </div>
