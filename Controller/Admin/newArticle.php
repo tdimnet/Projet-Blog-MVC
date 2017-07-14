19deca420a454a7ec56f2cd75e34dfa860466042
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-07-14 18:01:02 +0200

    Add new security verification for the admin panel

--- Controller/Admin/newArticle.php
+++ Controller/Admin/newArticle.php
@@ -15,22 +15,18 @@ if (isset($_SESSION['flashbag'])) {
   $flashMessage = getFlash();
 }
 
-
 // If a new article has been posted
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_SESSION['token']) AND isset($_POST['token']) AND !empty($_SESSION['token']) AND !empty($_POST['token']))) {
-
-  $Article = new Article();
-
-  $title = $_POST['titre'];
-  $episode = $_POST['episode'];
+  $title = trim(htmlspecialchars($_POST['title']));
+  $episode = trim(htmlspecialchars($_POST['episode']));
   $created_at = new \DateTime();
   $created_at = $created_at->format('Y-m-d H:i:s');
   $status = $_POST['status'];
-
   if (strlen($title) === 0 || strlen($episode) === 0) {
     addFlash('Votre article doit ne comporte pas tous les éléments nécessaires !');
     header('Location: index.php?Controller=Admin&&Vue=newArticle');
   } else {
+    $Article = new Article();
     $Article->setTitre($title);
     $Article->setEpisode($episode);
     $Article->setCreatedDate($created_at);
@@ -41,9 +37,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_SESSION['token']) AND isse
     // We inform the user that the article has been posted
     addFlash('Votre article a bien été soumis !');
     // Then we redirect the user
-    header('Location: index.php');
+    header('Location: index.php?Controller=Admin');
   }
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  require_once 'Vue\Admin\newArticle.php';
+} else {
+  addFlash('Vous n\'avez pas vous accès à votre espace personnalisé !');
+  header('Location: index.php');
 }
-
-
-require_once 'Vue\Admin\newArticle.php';
