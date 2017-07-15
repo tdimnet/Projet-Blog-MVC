11ddaef0f2869abc5fc035a7c20d9392e8afaf03
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-07-15 11:35:15 +0200

    Fix bug when submitting article and add token

--- Controller/Admin/newArticle.php
+++ Controller/Admin/newArticle.php
@@ -10,31 +10,32 @@ require_once 'Services/flashMessagesService.php';
 session_start();
 isConnected($_SESSION);
 
-unset($_SESSION['token']);
-$token = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
-$_SESSION['token'] = $token;
+$token = $_SESSION['token'];
 
 if (isset($_SESSION['flashbag'])) {
   $flashMessage = getFlash();
 }
 
 // If a new article has been posted
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_SESSION['token']) AND isset($_POST['token']) AND !empty($_SESSION['token']) AND !empty($_POST['token']))) {
-  $title = trim(htmlspecialchars($_POST['title']));
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_SESSION['token']) AND isset($_POST['token']) AND !empty($_SESSION['token']) AND !empty($_POST['token']) && $_SESSION['token'] === $_POST['token'])) {
+
+  $title = trim(htmlspecialchars($_POST['titre']));
   $episode = trim(htmlspecialchars($_POST['episode']));
   $created_at = new \DateTime();
   $created_at = $created_at->format('Y-m-d H:i:s');
   $status = $_POST['status'];
+
   if (strlen($title) === 0 || strlen($episode) === 0) {
     addFlash('Votre article doit ne comporte pas tous les éléments nécessaires !');
     header('Location: index.php?Controller=Admin&&Vue=newArticle');
+
   } else {
+
     $Article = new Article();
     $Article->setTitre($title);
     $Article->setEpisode($episode);
     $Article->setCreatedDate($created_at);
     $Article->setStatus($status);
-
     // We add the article into the data base
     addArticle($Article);
     // We inform the user that the article has been posted
