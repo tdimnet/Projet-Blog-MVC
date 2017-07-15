e5757549999b78adee040ab4f24ca0c2f77f79ab
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-07-15 11:31:37 +0200

    Fix bug with token and add new security when comparaison session token and post token

--- Controller/Admin/modifyArticle.php
+++ Controller/Admin/modifyArticle.php
@@ -9,9 +9,7 @@ require_once 'Services/flashMessagesService.php';
 session_start();
 isConnected($_SESSION);
 
-unset($_SESSION['token']);
-$token = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
-$_SESSION['token'] = $token;
+$token = $_SESSION['token'];
 
 $articleId = $_GET['id'];
 $article = findOne($articleId);
@@ -19,7 +17,7 @@ $article = findOne($articleId);
 if (isset($articleId) && !is_null($article)) {
   require_once 'Vue\Admin\modifyArticle.php';
 
-  if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_SESSION['token']) AND isset($_POST['token']) AND !empty($_SESSION['token']) AND !empty($_POST['token']))) {
+  if ($_SERVER['REQUEST_METHOD'] === 'POST' && (isset($_SESSION['token']) && isset($_POST['token']) && !empty($_SESSION['token']) && !empty($_POST['token']) && $_SESSION['token'] === $_POST['token'])) {
 
     $title = trim(htmlspecialchars($_POST['titre']));
     $episode = trim(htmlspecialchars($_POST['episode']));
