b131248ca868a091263c0819d06c58b8dc26d048
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-07-16 22:19:51 +0200

    Correct wording and remove bug

--- Vue/Blog/leaveComment.php
+++ Vue/Blog/leaveComment.php
@@ -5,11 +5,11 @@
   <form class="" method="post">
     <div class="form-group">
       <label for="fullname">Votre nom :</label>
-      <input id="fullname" class="form-control" type="text" name="name" placeholder="Votre nom :">
+      <input id="fullname" class="form-control" type="text" name="name" placeholder="Exemple : Marc">
     </div>
     <div class="form-group">
       <label for="comment">Votre commentaire :</label>
-      <textarea id="comment" class="form-control" name="comment" placeholder="Votre commentaire"></textarea>
+      <textarea id="comment" class="form-control" name="comment" placeholder="Exemple : un super article"></textarea>
     </div>
     <button class="btn btn-default" type="submit" name="button">
       Envoyer votre commentaire
